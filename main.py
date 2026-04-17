857fec6514359e35939635478af6087326bcd21a
Bhoomi <bhoomime14@gmail.com>
2026-04-17 17:50:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # ----------- FAKE DATABASE (for now) -----------
 DB = []
 
